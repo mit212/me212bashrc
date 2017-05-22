b18eb9aabe8dada87528f70e72889411aae1e7bd
Ryan Fish <ryanfishme@gmail.com>
2017-05-22 11:16:55 -0400

    integrated bashrc install from install.sh
    that way there's just one command for installation

--- install_software.sh
+++ install_software.sh
@@ -14,6 +14,21 @@ function ask {
     fi
 }
 
+if [ "$#" == 0 ] || [ "$1" == "BASH" ]; then
+  echo "Initialize the bashrc"
+  if [ -f $HOME/.bashrc  ]; then
+    mv $HOME/.bashrc $HOME/.bashrc_bak
+  fi
+  if [ -f $HOME/.bash_aliases  ]; then
+    mv $HOME/.bash_aliases $HOME/.bashrc_bak
+  fi
+  ln -s "$HOME/me212bashrc/.bashrc" "$HOME/.bashrc"
+  ln -s "$HOME/me212bashrc/.bash_aliases" "$HOME/.bash_aliases"
+  cp -r $HOME/me212bashrc/.config $HOME/.config
+  cp -r $HOME/me212bashrc/.local $HOME/.local
+  sudo cp $HOME/me212bashrc/etc/rc.local /etc/rc.local
+fi
+
 if [ "$#" == 0 ] || [ "$1" == "APT" ]; then
     echo "Install useful packages from apt-get"
     sudo apt-get update
